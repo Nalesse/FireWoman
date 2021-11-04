f2b2f1b87171f87d8b8bf05fdb8027ccdbd2ca71
Christian Hotte <christianwhotte@gmail.com>
2021-11-04 01:16:40 -0400

    Orientation Detection Worked Out
    -Orientation variable is now collected at a set interval, and represents normal of phone screen (not normalized)
    -Debug lines have been added for raw orientation (white) and smoothed orientation (red)
    -Smoothed orientation is taken as average of set number of previous orientation vectors stored in memory (can be changed in inspector)

--- Assets/3 Scripts/PlayerController.cs
+++ Assets/3 Scripts/PlayerController.cs
@@ -7,23 +7,53 @@ public class PlayerController : MonoBehaviour
     //Description: Gets information from phone and controls player character
 
     //Objects & Components:
-    private Transform orientator; //TEMP: thing to show phone orientation
+
+    //Settings:
+    public float checkOrientFreq;  //How frequently (in seconds between checks) the phone's orientation is checked
+    public int orientAvgGroupSize; //How many orientation readings to remember at a time and average between (to smooth orientation data)
+    public bool debugStuff;        //Used to turn debug logs and lines off and on
 
     //Input Vars:
-    private Vector3 orientation; //Normalized vector representing the spatial direction the back of the phone is facing (when phone is being held still)
+    private Vector3 orientation;    //Normalized vector representing the spatial direction the screen of the phone is facing (when phone is being held still)
+    private Vector3 avgOrientation; //Average of the last [orientationSmoothingGroupSize] orientation vectors
+
+    //Memory Vars:
+    private List<Vector3> orientationMem = new List<Vector3>(); //List of past orientation readings used for smoothing
 
     //RUNTIME METHODS:
     private void Awake()
     {
-        orientator = transform.GetChild(0).GetChild(0); //TEMP
+        StartCoroutine(OrientationDataChecker()); //Start phone orientation checks (will repeat at given frequency for program runtime)
     }
     private void Update()
     {
-        //Input Stuff:
-        orientation = Input.acceleration; //Get acceleration from phone
-
         //Input Debugging:
-        orientator.eulerAngles = (orientation * Mathf.Rad2Deg) + transform.GetChild(0).eulerAngles; //Move orientator to represent phone orientation (offset by container rotation so that it can be easily re-positioned)
-        print(Input.acceleration);
+        if (debugStuff)
+        {
+            Debug.DrawLine(transform.position, orientation);
+            Debug.DrawLine(transform.position, avgOrientation, Color.red);
+            print(orientation);
+        }
+    }
+    IEnumerator OrientationDataChecker()
+    {
+        //Function: Checks orientation data at a set interval, and fills in input (and memory) variables related to phone orientation
+
+        for (;;) //Run code indefinitely
+        {
+            //Get Orientation Data:
+            Vector3 rawOrientation = Input.acceleration; //Get acceleration from phone (should be relatively normalized to gravity, floats around 1 when phone is still, and points down from bottom of phone)
+            orientation = Quaternion.Euler(90, 0, 0) * rawOrientation; //Rotate raw orientation vector so that it points in the direction the phone is facing
+
+            //Update Orientation Average:
+            orientationMem.Add(orientation); //Add orientation to memory
+            if (orientationMem.Count > orientAvgGroupSize) orientationMem.RemoveAt(0); //Trim memory to ensure it stays within desired group size (allowing memory to be tracked across specific length of time)
+            Vector3 totalOrientation = new Vector3(); //Initialize empty vector to store totals of all orientation variables
+            foreach (Vector3 memOrient in orientationMem) totalOrientation += memOrient; //Scrub through all orientation vectors in memory and add them together
+            avgOrientation = totalOrientation / orientationMem.Count; //Get average orientation in memory by dividing totals of all orientation vectors by number of vectors in memory
+
+            //Wait for Given Interval:
+            yield return new WaitForSeconds(checkOrientFreq); //Check orientation again after the frame closest to when given amount of seconds has passed
+        }
     }
 }
