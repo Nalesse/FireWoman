358d9309620aafbaa0d10f7e2e8f803bd2c9aed4
Christian Hotte <christianwhotte@gmail.com>
2021-11-06 08:49:09 -0400

    Core Animation Added
    -More granular control over phone tilt input added, sensitivity slider implemented to allow game to be played more comfortably
    -Character lean animation added, program scrubs through clip based on input-derived lean value
    -Background added (with temp planes that will later be replaced by animated sprites)
    -Background planes move independently, sliding toward and away the player based on modifiable animation curve
    -Extents of background slide are able to be set in the inspector, allowing them to be modified independently of the animation curve which puts the planes in position
    -Base orientation vector added for calibration, now the user can set this value to whichever direction they want to be neutrally holding their phone in (we could make this a simple button)

--- Assets/3 Scripts/LevelManager.cs
+++ Assets/3 Scripts/LevelManager.cs
@@ -10,39 +10,52 @@ public class LevelManager : MonoBehaviour
 
     //Objects & Components:
     private Transform backgroundGroup; //TEMP reference to level for testing purposes
+    private Animator playerAnimator;   //Reference to player animator
 
     //Settings:
     [Header("Settings:")]
-    public Vector2 slideRange; //Maximum distance the background moves vertically and horizontally
-    public Vector3 basePhoneOrientation; //Orientation vector at which system assumes player is standing neutrally (should be able to be calibrated for)
+    [Range(0, 1)] public float tiltSensitivity; //Increasing this will decrease the amount phone has to be tilted to fully tilt the player
+    public Vector2 slideRange;                  //The two ends of the range background elements slide when phone is being tilted
+    public AnimationCurve stageSlideCurve;      //Describes how stage slides into position depending on lean value
+    public Vector3 basePhoneOrientation;        //Orientation vector at which system assumes player is standing neutrally (should be able to be calibrated for)
 
     //Memory Vars:
     private Vector3 backgroundOrigPos; //Original position of level background elements
 
     private void Awake()
     {
-        backgroundGroup = transform.GetChild(0);      //Get background group container object
+        //Get Objects & Components:
+        backgroundGroup = transform.GetChild(0); //Get background group container object
+        playerAnimator = transform.GetChild(1).GetComponentInChildren<Animator>(); //Get player animator
+
         backgroundOrigPos = backgroundGroup.position; //Get starting position of background element
     }
     private void Update()
     {
-        PositionBackground(); //Move background elements
+        AnimateScene(); //Move background elements
     }
 
-    private void PositionBackground()
+    private void AnimateScene()
     {
         //Function: Moves/animates background depending on player input
 
-        //Get Workable Input Variable:
+        //Get & Refine Input Variable:
         Vector3 rawOrientation = InputManager.manager.avgOrientation; //Get smoothed phone orientation from inputManager
         Vector3 relativeAngles = Quaternion.FromToRotation(basePhoneOrientation, rawOrientation).eulerAngles; //Get eulers for angles between base (neutral) vector and current phone alignment
         relativeAngles = new Vector3(HotteMath.NormalizeAngle(relativeAngles.x), HotteMath.NormalizeAngle(relativeAngles.y), HotteMath.NormalizeAngle(relativeAngles.z)); //Put all angles in -180 to 180 range
-        //print("Relative = " + relativeAngles);
+        float tiltIntensity = relativeAngles.x / 90; //Get intensity and direction in which phone is tilting (between -1 and 1)
+        tiltIntensity = Mathf.Clamp(HotteMath.Map(tiltIntensity, -(1 - tiltSensitivity), 1 - tiltSensitivity, -1, 1), -1, 1); //Magnify phone tilt based on given setting
 
-        //Determine Background Translation:
-        float slideIntensity = relativeAngles.x / 90; //Get intensity and direction in which background should slide (within range 0-1) based on vertical angle of phone
-        //print("Slide = " + slideIntensity);
-        Vector3 targetPos = backgroundOrigPos + new Vector3(0, slideRange.y * slideIntensity, 0); //Get target vector for background (limit full range based on known slide intensity)
-        backgroundGroup.transform.position = targetPos; //TEMP: Just snap background to target position
+        //Animate Background:
+        float waterSlide = Mathf.Lerp(-slideRange.x, -slideRange.y, stageSlideCurve.Evaluate(-tiltIntensity)); //Find target elevation of water side of level
+        float fireSlide = Mathf.Lerp(slideRange.x, slideRange.y, stageSlideCurve.Evaluate(tiltIntensity));    //Find target elevation of fire side of level
+        Vector3 targetPosWater = backgroundOrigPos + new Vector3(0, waterSlide, 0); //Get target vector for water side of level
+        Vector3 targetPosFire = backgroundOrigPos + new Vector3(0, fireSlide, 0); //Get target vector for fire side of level
+        backgroundGroup.transform.GetChild(0).position = targetPosWater; //Move water background to target position
+        backgroundGroup.transform.GetChild(1).position = targetPosFire;  //Move fire background to target position
+
+        //Animate Player:
+        float animatorTiltIntensity = 1f - ((tiltIntensity / 2) + 0.5f); //Get adjusted tilt intensity between 0 and 1 (and flipped)
+        playerAnimator.SetFloat("Frame", animatorTiltIntensity); //Set lean based on tilt value
     }
 }
